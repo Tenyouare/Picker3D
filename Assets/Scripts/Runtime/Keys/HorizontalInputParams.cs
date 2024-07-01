936f3d1b5bd82fec0c376fc0d39e7555c855533f
Tenyouare <onurtorun42@hotmail.com>
2024-07-01 17:17:31 +0300

    Core UI System Finalized

---
+++ Assets/Scripts/Runtime/Keys/HorizontalInputParams.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct HorizontalInputParams
+{
+    public float2 ClampValues;
+    public float HorizontalValue;
+}
